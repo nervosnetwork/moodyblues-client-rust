05f0abe5f556985dac4ba8f7029b6e746bf6b92d
homura <homura.dev@gmail.com>
2019-12-09 18:11:21 +0800

    docs: explain the event struct

--- src/event.rs
+++ src/event.rs
@@ -4,43 +4,59 @@ use serde_json::Value;
 
 pub type Json = Value;
 
-/// 标记事件的类型
+/// Event type definition
 #[derive(Debug, Serialize, Deserialize)]
 pub enum EventType {
-    /// 提案事件，可用于标记发起提案或者收到提案
+    /// mark the event as a propose event
     Propose { proposer: String, hash: String },
-    /// 投票事件，可用于标记投票或者收到投票
+    /// mark as a vote event
     Vote { voter: String, hash: String },
-    /// 错误事件
+    /// mark as an error event
     Error,
-    /// 关键事件，在一个 round 中，keyframe 标志着一个 step 的开始
+    /// mark the event as a keyframe
     Keyframe { frame_info: Keyframe },
-    /// 自定义事件
+    /// mark the event as a custom event
     Custom,
 }
 
-/// 标识关键事件
+/// A keyframe is a start point of a phase, the Tendermint-like algorithm always
+/// through a series of steps to reach a consensus like this
+/// ```graph
+/// +-----------------------------------------------+
+/// |                 epoch 1                       |
+/// +--+---------------------------------------+----+
+///    |              round 1                  |
+///    +-+-----------+------------+----------+-+
+///      |   step 1  |   step 2   |  step .. |
+///      +-----------+------------+----------+
+/// ```
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Keyframe {
-    /// 当开始新的 Epoch 时的事件标识
+    /// mark the event as starts of epoch
     NewEpoch { epoch_id: u64 },
-    /// 当开始新一轮投票时的事件标识
+    /// mark the event as starts of round
     NewRound { round_id: u64 },
-    /// 当开始新一个 Step 时的事件表标识
+    /// mark the event as starts of step
     NewStep { step_name: String },
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TraceEvent {
-    /// 事件名，用于标识事件，需保证不同事件有不同名称
+    /// event name
     pub event_name: String,
-    /// 事件的类型
+    /// event type
     pub event_type: EventType,
-
-    /// 额外关心的指标，使用 json 格式表示
-    /// 注意：tag 中有预留的关键key
-    /// error: bool 默认false，表示是否为错误事件
-    /// log.message: Option<String>  日志信息
-    /// log.stack: Option<String> 默认为空 堆栈信息
+    /// metrics of the event, for example, I want to check the order of transactions is correct, and
+    /// I want to show what params in the request, so emit an event like this
+    /// ```json
+    /// {
+    ///   "event_name" : "request_check_transaction_order",
+    ///   "event_type" : "custom",
+    ///   "tag": {
+    ///     "epoch_id": 1,
+    ///     "transactions": ["0x...", "0x..."]
+    ///   }
+    /// }
+    /// ```
     pub tag: Option<Json>,
 }
